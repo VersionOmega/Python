63d67fe06f2ed1770ede5830a32815a7372aa132
Daniel Roberts <me@Daniels-MacBook-Pro.local>
2020-03-20 21:58:26 +0000

    Initial

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+import sys
+
+
+class Application:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.running = True
+
+        pygame.init()
+
+        self.display = pygame.display.set_mode(
+            (self.width, self.height), pygame.HWSURFACE | pygame.DOUBLEBUF)
+
+        pygame.display.set_caption("RPG II")
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    game = Application(800, 650)
